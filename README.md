bb0d027d0c478d56123ec455b53b3472a019cdb5
my <jvckigh@gmail.com>
2021-12-27 13:57:13 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-blind
+# blind
+
+## 1. Spec
+### Backend
+* Spring boot 2.6.2
+* JPA
+
+### Frontend
+* Vue.js
---
+++ src/test/java/com/geon/blind/BlindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geon.blind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
